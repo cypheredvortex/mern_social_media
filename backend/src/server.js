930984bb866ea5de88a9363c22309adabe6e53c8
mrxcode <alimalik21realms@gmail.com>
2025-12-08 03:00:01 +0100

    second commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,18 @@
 import express from "express"
 import postRoutes from "./routes/postRoutes.js"
+import activityLogsRoutes from "./routes/activityLogsRoutes.js"
+import commentRoutes from "./routes/commentRoutes.js"
+import followRoutes from "./routes/followRoutes.js"
+import likeRoutes from "./routes/likeRoutes.js"
+import mediaRoutes from "./routes/mediaRoutes.js"
+import messageRoutes from "./routes/messageRoutes.js"
+import notificationRoutes from "./routes/notificationRoutes.js"
+import profileRoutes from "./routes/profileRoutes.js"
+import reportRoutes from "./routes/reportRoutes.js"
+import searchHistoryRoutes from "./routes/searchHistoryRoutes.js"
+import shareRoutes from "./routes/shareRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+import userSettingsRoutes from "./routes/userSettingsRoutes.js"
 import { connectDB } from "./config/db.js"
 import dotenv from "dotenv"
 
@@ -16,6 +29,19 @@ app.use((req,res,next)=>{
 })
 
 app.use("/api/posts",postRoutes);
+app.use("/api/activity-logs",activityLogsRoutes);
+app.use("/api/comments",commentRoutes);
+app.use("/api/follows",followRoutes);
+app.use("/api/likes",likeRoutes);
+app.use("/api/media",mediaRoutes);
+app.use("/api/messages",messageRoutes);
+app.use("/api/notifications",notificationRoutes);
+app.use("/api/profiles",profileRoutes);
+app.use("/api/reports",reportRoutes);
+app.use("/api/search-histories",searchHistoryRoutes);
+app.use("/api/shares",shareRoutes);
+app.use("/api/users",userRoutes);
+app.use("/api/user-settings",userSettingsRoutes);
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
